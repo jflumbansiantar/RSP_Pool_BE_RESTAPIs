0ff7ad4834a6961d470f007c1510ca3e05ea132a
Julia Fransiska L <jflumbansiantar@gmail.com>
2020-12-18 22:56:08 +0700

    10 out of 14 endpoints working

--- models/rooms.js
+++ models/rooms.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      rooms.hasMany(models.bookings)
+      // rooms.hasMany(models.bookings)
     }
   };
   rooms.init({
